2c86e37a2ad4dccd4874e9c87d6b37680d21527c
brianjo94 <brianjo1994@gmail.com>
2023-01-10 21:25:04 -0500

    Added Navbar using template inheritance. Used Bootstrap for Navbar

--- patients/models.py
+++ patients/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class PatientName(models.Model):
+    """Registering patient name"""
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text 
--- patients/urls.py
+++ patients/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'patients'
 urlpatterns =[
     path('', views.homepage, name='homepage')
 ]
